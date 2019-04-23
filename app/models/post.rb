33203f56c7205a4d18257a8b8f083def122e6626
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-23 16:58:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,18 @@
 class Post < ActiveRecord::Base
-  has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :post_categories #all this does is add methods
+  has_many :categories, through: :post_categories #all this does is add methods
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  #accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        if !self.categories.include?(:category)
+          self.post_categories.build(category: category)
+        end
+      end
     end
   end
 end
