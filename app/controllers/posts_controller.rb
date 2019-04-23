38f6147f71034fb2cf1b0051ee30c97b46cb34d6
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-23 17:11:29 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
   end
 
   def create 
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to @post
   end
 
   private
